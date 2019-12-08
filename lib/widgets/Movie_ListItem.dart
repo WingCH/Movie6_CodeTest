d2d82668d181d99ec2f421c74fcb6edb30536fe0
WingCH <g1191993@study.ouhk.edu.hk>
2019-12-08 16:30:04 +0800

    Movie List rating

--- lib/widgets/Movie_ListItem.dart
+++ lib/widgets/Movie_ListItem.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 // ignore: camel_case_types
 class MovieListItem extends StatelessWidget {
@@ -11,15 +12,18 @@ class MovieListItem extends StatelessWidget {
     return Row(
       children: <Widget>[
         _MovieListImage(
-            'https://storage.hkmovie6.com/prod/movie/movies_50517-2019-10-24-050523046.jpg')
+            'https://storage.hkmovie6.com/prod/movie/movies_50517-2019-10-24-050523046.jpg'),
+        _MovieListRating(3.9),
       ],
     );
   }
 }
 
+
 class _MovieListImage extends StatelessWidget {
   final String url;
-  _MovieListImage(this.url);
+
+  _MovieListImage(this.url, {Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -30,5 +34,42 @@ class _MovieListImage extends StatelessWidget {
           alignment: Alignment.center,
           image: NetworkImage(url),
         ));
+  }
+}
+
+class _MovieListRating extends StatelessWidget {
+  final double rating;
+
+  const _MovieListRating(
+      this.rating, {
+        Key key,
+      }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Text(
+          '$rating',
+          style: TextStyle(
+              fontSize: 50,
+              fontWeight: FontWeight.w800,
+              color: Color.fromRGBO(253, 220, 11, 1)),
+        ),
+        RatingBar(
+          itemSize: 50 / 5,
+          initialRating: rating,
+          direction: Axis.horizontal,
+          allowHalfRating: true,
+          ignoreGestures: true,
+          itemCount: 5,
+          itemBuilder: (context, _) => Icon(
+            Icons.star,
+            color: Colors.amber,
+          ),
+          onRatingUpdate: (double value) {},
+        )
+      ],
+    );
   }
 }
