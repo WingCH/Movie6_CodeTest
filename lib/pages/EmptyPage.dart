644e57d6292b287cb2da1a1e6fc1b0458f8c5cc9
WingCH <g1191993@study.ouhk.edu.hk>
2019-12-08 14:18:14 +0800

    Empty page

---
+++ lib/pages/EmptyPage.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class EmptyPage extends StatefulWidget {
+  @override
+  _EmptyPageState createState() => _EmptyPageState();
+}
+
+class _EmptyPageState extends State<EmptyPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        child: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text('Empty Page', style: Theme.of(context).textTheme.title)
+        ],
+      ),
+    ));
+  }
+}
